89390d7462e45d78de61e555b9e405170b159942
ericlovesmath <ericlovesmath@gmail.com>
2021-03-22 20:03:05 -0700

    Query added

--- API-Fetch/Core-Api.py
+++ API-Fetch/Core-Api.py
@@ -9,14 +9,28 @@ with open("API_Keys.json", "r") as APIKEY:
 
 CORE_KEY = KEYS["Core-Key"]
 
-PAGE = 1  # Pages 1 - 100
-PAGESIZE = 10  # 10 results per page, 10-100
+QUERY = input("Query: ")
 
-URL = f"https://core.ac.uk:443/api-v2/search/Hello?page={PAGE}&pageSize={PAGESIZE}&apiKey={CORE_KEY}"  # noqa
+limit = 10  # 1-100 articles fetched
+metadata = "true"
+fulltext = "false"
+citations = "false"
+similar = "false"
+duplicate = "false"
+urls = "false"
+faithfulMetadata = "false"
 
-res = requests.get(URL)
+URL = f"https://core.ac.uk:443/api-v2/articles/similar?limit={limit}&metadata={metadata}&fulltext={fulltext}&citations={citations}&similar={similar}&duplicate={duplicate}&urls={urls}&faithfulMetadata={faithfulMetadata}&apiKey={CORE_KEY}"  # noqa
+
+res = requests.post(
+    URL, data=QUERY.encode("utf-8"), headers={"Content-Type": "text/plain"}
+)
 res = res.json()
-print(json.dumps(res, indent=4))
+
+for paper in res["data"]:
+    print("\n" + paper["title"])
+
+# print(json.dumps(res, indent=4))
 
 with open("out.json", "w") as outfile:
     json.dump(res, outfile, indent=4)
