969087a61f23d749005bd52a08fa991863a5c772
Dylan Zhang <dylan.zhang@gmail.com>
2021-03-27 16:27:41 -0700

    changes

--- bert/article_query.py
+++ bert/article_query.py
@@ -1,15 +1,29 @@
 import json
 import requests
+import urllib.parse
 from django.conf import settings
 
 CORE_KEY = settings.CORE_KEY
 
-def get_queries(queries, limit, metadata, fulltext, citations, similar, duplicate, urls, faithfulMetadata):
-	url = f"https://core.ac.uk:443/api-v2/articles/search?metadata={metadata}&fulltext={fulltext}&citations={citations}&similar={similar}&duplicate={duplicate}&urls={urls}&faithfulMetadata={faithfulMetadata}&apiKey={CORE_KEY}" 
-	data = []
+def get_queries(query, page_limit, params):
+	endpoint = "https://core.ac.uk:443/api-v2/articles/search/"
+	params['apiKey'] = CORE_KEY
 	
-	for query in queries:
-		data.append({"query": '"' + query + '" AND description:*', "page":1, "pageSize": limit, "language.name":"English"})
-	
-	res = requests.post(url, data = json.dumps(data), headers={"Content-Type": "text/plain"})
-	return res.json()
+	real_query = '(title:('+query+') OR description:('+query+') OR fullText:('+query+')) AND description:*'
+	# language.name:English doesnt work
+
+	url = endpoint + urllib.parse.quote(real_query)
+	print(url)
+
+	all_articles = []
+	result = requests.get(url, params=params).json()
+	all_articles.extend(result['data'])
+	if (result['totalHits']>params['pageSize']):
+		pages = int(result['totalHits']/params['pageSize'])  #rounds down
+		if (pages>page_limit):
+			pages=page_limit
+		for i in range(2, pages+1):
+			params['page'] = i
+			result = requests.get(url, params=params).json()
+			all_articles.extend(result["data"])
+	return all_articles
